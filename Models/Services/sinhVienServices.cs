a83ceae3a78542009cd6ca8f9041b6944c6f4d6a
nguyenkiman <nguyenkiman510@gmail.com>
2021-07-25 08:25:47 +0700

    code theo struct như ri nghe, controller sẽ dùng services nghe

---
+++ Models/Services/sinhVienServices.cs
@@ -0,0 +1,32 @@
+﻿using Models.DAO;
+using Models.EF;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Services
+{
+    public class sinhVienServices
+    {
+        sinhVienDAO sinhVienDAO = new sinhVienDAO();
+        public sinhVien findByMaSinhVien(String maSinhVien)
+        {
+            return sinhVienDAO.findByMaSinhVien(maSinhVien);
+        }
+        public IPagedList<sinhVien> findByMaLop(String maLop,int pageIndex)
+        {
+            return sinhVienDAO.findByMaLop(maLop).ToPagedList(pageIndex, Constraints.Common.PAGE_SIZE);
+        }
+        public IPagedList<sinhVien> findByMaNganh(String maNganh, int pageIndex)
+        {
+            return sinhVienDAO.findByMaNganh(maNganh).ToPagedList(pageIndex, Constraints.Common.PAGE_SIZE);
+        }
+        public int checkLogin(String maSinhVien, String matKhau)
+        {
+            return sinhVienDAO.checkLogin(maSinhVien, matKhau);
+        }
+    }
+}
