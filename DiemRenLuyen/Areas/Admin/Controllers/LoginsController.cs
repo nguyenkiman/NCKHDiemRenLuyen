b4853008600779f6b8461eee3ee68a2d7db24729
xpekexoan <yourshapw1989@gmail.com>
2021-07-26 00:30:26 +0700

    Login Admin

---
+++ DiemRenLuyen/Areas/Admin/Controllers/LoginsController.cs
@@ -0,0 +1,48 @@
+﻿using DiemRenLuyen.Areas.Admin.Model;
+using Models.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiemRenLuyen.Areas.Admin.Controllers
+{
+    public class LoginsController : Controller
+    {
+        // GET: Admin/Logins
+        public ActionResult Index(LoginModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new adminDAO();
+                var result = dao.checkLogin(user.UserName, user.PassWord);
+                if(result == 0)
+                {
+                    ModelState.AddModelError("", "Tài khoản không tồn tại");
+                }
+                else
+                {
+                    if (result == 2)
+                    {
+                        ModelState.AddModelError("", "Đăng nhập thành công");
+                        Session.Add(Models.Constraints.Common.USER_SESSION, user);
+                        return RedirectToAction("Index", "baoHiemYTes");
+                    }
+                    else
+                    {
+                        if (result == 1)
+                        {
+                            ModelState.AddModelError("", "Mật khẩu sai");
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", "Tài khoản đã bị khoá");
+                        }
+                    }
+                }
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ Models/DAO/adminDAO.cs
@@ -0,0 +1,41 @@
+﻿using Models.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DAO
+{
+    public class adminDAO
+    {
+        Db db = new Db();
+        public int checkLogin(String maAdmin, String matKhau)
+        {
+            admin admin = db.admins.Find(maAdmin);
+            if(admin is null)
+            {
+                return Constraints.Common.ACCOUNT_NOT_EXISTS;
+                
+            }
+            else
+            {
+                if (admin.trangThai == 0)
+                {
+                    return Constraints.Common.BLOCK;
+                }
+                else
+                {
+                    if (admin.matKhau.Equals(matKhau))
+                    {
+                        return Constraints.Common.LOGIN_SUCCESS;
+                    }
+                    else
+                    {
+                        return Constraints.Common.INVALID_PASSWORDS;
+                    }
+                }
+            }
+        }
+    }
+}
