2b79dc833f8830432e7ba4458889ed56c52e3365
xpekexoan <yourshapw1989@gmail.com>
2021-07-25 21:39:22 +0700

    add giao dien admin

---
+++ DiemRenLuyen/Areas/Admin/Controllers/GiangVienController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiemRenLuyen.Areas.Admin.Controllers
+{
+    public class GiangVienController : Controller
+    {
+        // GET: Admin/GiangVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/EF/DbContext.cs
@@ -0,0 +1,100 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Models.EF
+{
+    public partial class DbContext : DbContext
+    {
+        public DbContext()
+            : base("name=DbContext")
+        {
+        }
+
+        public virtual DbSet<admin> admins { get; set; }
+        public virtual DbSet<baoHiemYTe> baoHiemYTes { get; set; }
+        public virtual DbSet<canBoLop> canBoLops { get; set; }
+        public virtual DbSet<chiTietPhieuCham> chiTietPhieuChams { get; set; }
+        public virtual DbSet<danhMucDanhGia> danhMucDanhGias { get; set; }
+        public virtual DbSet<diemHocTap> diemHocTaps { get; set; }
+        public virtual DbSet<giangVien> giangViens { get; set; }
+        public virtual DbSet<giaoVienChuNhiem> giaoVienChuNhiems { get; set; }
+        public virtual DbSet<hocKi> hocKis { get; set; }
+        public virtual DbSet<hocPhi> hocPhis { get; set; }
+        public virtual DbSet<khoa> khoas { get; set; }
+        public virtual DbSet<lop> lops { get; set; }
+        public virtual DbSet<nganh> nganhs { get; set; }
+        public virtual DbSet<noiDungDanhGia> noiDungDanhGias { get; set; }
+        public virtual DbSet<phieuChamDiem> phieuChamDiems { get; set; }
+        public virtual DbSet<sinhVien> sinhViens { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<admin>()
+                .HasMany(e => e.hocKis)
+                .WithOptional(e => e.admin)
+                .HasForeignKey(e => e.nguoiTao);
+
+            modelBuilder.Entity<giangVien>()
+                .HasMany(e => e.giaoVienChuNhiems)
+                .WithRequired(e => e.giangVien)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<hocKi>()
+                .HasMany(e => e.baoHiemYTes)
+                .WithRequired(e => e.hocKi)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<hocKi>()
+                .HasMany(e => e.diemHocTaps)
+                .WithRequired(e => e.hocKi)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<hocKi>()
+                .HasMany(e => e.hocPhis)
+                .WithRequired(e => e.hocKi)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<lop>()
+                .HasMany(e => e.canBoLops)
+                .WithRequired(e => e.lop)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<lop>()
+                .HasMany(e => e.giaoVienChuNhiems)
+                .WithRequired(e => e.lop)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<noiDungDanhGia>()
+                .HasMany(e => e.chiTietPhieuChams)
+                .WithRequired(e => e.noiDungDanhGia)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<phieuChamDiem>()
+                .HasMany(e => e.chiTietPhieuChams)
+                .WithRequired(e => e.phieuChamDiem)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<sinhVien>()
+                .HasMany(e => e.baoHiemYTes)
+                .WithRequired(e => e.sinhVien)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<sinhVien>()
+                .HasMany(e => e.canBoLops)
+                .WithRequired(e => e.sinhVien)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<sinhVien>()
+                .HasMany(e => e.diemHocTaps)
+                .WithRequired(e => e.sinhVien)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<sinhVien>()
+                .HasMany(e => e.hocPhis)
+                .WithRequired(e => e.sinhVien)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
