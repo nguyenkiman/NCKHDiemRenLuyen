d150b0fece6824510ea887cbcca1172488fde5e9
nguyenkiman <nguyenkiman510@gmail.com>
2021-07-26 11:07:51 +0700

    login task

--- Models/Services/phieuChamDiemServices.cs
+++ Models/Services/phieuChamDiemServices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Models.DAO;
+using Models.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,25 @@ using System.Threading.Tasks;
 
 namespace Models.Services
 {
-    class phieuChamDiemServices
+    public class phieuChamDiemServices
     {
+        noiDungDanhGiaDAO noiDungDanhGiaDAO = new noiDungDanhGiaDAO();
+        const int BHYT = 1;
+        public phieuChamDiem generateNewPhieuChamDiem(String masv)
+        {
+            phieuChamDiem newPhieuChamDiem = new phieuChamDiem();
+            newPhieuChamDiem.maSinhVien = masv;
+            List<chiTietPhieuCham> chiTietPhieuChams = new List<chiTietPhieuCham>();
+            IQueryable<noiDungDanhGia> noiDungDanhGias = noiDungDanhGiaDAO.findAll();
+            foreach(noiDungDanhGia noiDung in noiDungDanhGias)
+            {
+                chiTietPhieuCham chiTietPhieuCham = new chiTietPhieuCham();
+                chiTietPhieuCham.maNoiDung = noiDung.maNoiDung;
+                
+                if(noiDung.maNoiDung == 1) { }
+
+            }
+            return newPhieuChamDiem;
+        }
     }
 }
