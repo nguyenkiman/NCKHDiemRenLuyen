31a9a617f0902de873293309fd4b1cda4b14460b
nguyenhuuvong-nhv <71600841+nguyenhuuvong-nhv@users.noreply.github.com>
2021-07-27 13:07:24 +0700

    add import file exel to sql

--- DiemRenLuyen/Areas/Admin/Controllers/sinhViensController.cs
+++ DiemRenLuyen/Areas/Admin/Controllers/sinhViensController.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Data.OleDb;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using LinqToExcel;
 using Models.EF;
+using ExcelLibrary;
+using System.IO;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace DiemRenLuyen.Areas.Admin.Controllers
 {
@@ -39,8 +46,8 @@ namespace DiemRenLuyen.Areas.Admin.Controllers
         // GET: Admin/sinhViens/Create
         public ActionResult Create()
         {
-            ViewBag.maLop = new SelectList(db.lop, "maLop", "maNganh");
-            
+            ViewBag.maLop = new SelectList(db.lop, "maLop", "maLop");
+
             return View();
         }
 
@@ -96,33 +103,17 @@ namespace DiemRenLuyen.Areas.Admin.Controllers
         }
 
         // GET: Admin/sinhViens/Delete/5
+        [HttpGet]
         public ActionResult Delete(string id)
         {
 
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            sinhVien sinhVien = db.sinhVien.Find(id);
-            if (sinhVien == null)
-            {
-                return HttpNotFound();
-            }
-            return View(sinhVien);
-        }
-
-        // POST: Admin/sinhViens/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            
             sinhVien sinhVien = db.sinhVien.Find(id);
             sinhVien.trangThai = 0;
             db.Entry(sinhVien).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
 
         protected override void Dispose(bool disposing)
         {
@@ -135,8 +126,108 @@ namespace DiemRenLuyen.Areas.Admin.Controllers
 
         public ActionResult CreateExel()
         {
-          
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateExel(sinhVien users, HttpPostedFileBase FileUpload)
+        {
+
+            List<string> data = new List<string>();
+            if (FileUpload != null)
+            {
+                // tdata.ExecuteCommand("truncate table OtherCompanyAssets");
+                if (FileUpload.ContentType == "application/vnd.ms-excel" || FileUpload.ContentType == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                {
+                    string filename = FileUpload.FileName;
+                    string targetpath = Server.MapPath("~/Content/Doc/");
+                    FileUpload.SaveAs(targetpath + filename);
+                    string pathToExcelFile = targetpath + filename;
+                    var connectionString = "";
+                    if (filename.EndsWith(".xls"))
+                    {
+                        connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=Excel 8.0;", pathToExcelFile);
+                    }
+                    else if (filename.EndsWith(".xlsx"))
+                    {
+                        connectionString = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0 Xml;HDR=YES;IMEX=1\";", pathToExcelFile);
+                    }
+
+                    var adapter = new OleDbDataAdapter("SELECT * FROM [Sheet1$]", connectionString);
+                    var ds = new DataSet();
+                    adapter.Fill(ds, "ExcelTable");
+                    DataTable dtable = ds.Tables["ExcelTable"];
+                    string sheetName = "Sheet1";
+                    var excelFile = new ExcelQueryFactory(pathToExcelFile);
+                    var artistAlbums = from a in excelFile.Worksheet<sinhVien>(sheetName) select a;
+                    foreach (var a in artistAlbums)
+                    {
+                        try
+                        {
+                            if (a.maSinhVien != "" && a.matKhau != "" && a.tenSinhVien != "" && a.soDienThoai != "" && a.gmail != "" && a.gioiTinh != null && a.trangThai != null && a.maLop != "")
+                            {
+                                sinhVien TU = new sinhVien();
+                                TU.maSinhVien = a.maSinhVien;
+                                TU.matKhau = a.matKhau;
+                                TU.tenSinhVien = a.tenSinhVien;
+                                TU.ngaySinh = a.ngaySinh;
+                                TU.soDienThoai = a.soDienThoai;
+                                TU.gmail = a.gmail;
+                                TU.gioiTinh = a.gioiTinh;
+                                TU.trangThai = a.trangThai;
+                                TU.maLop = a.maLop;
+                                db.sinhVien.Add(TU);
+                                db.SaveChanges();
+                            }
+                            else
+                            {
+                                data.Add("<ul>");
+                                if (a.maSinhVien == "" || a.maSinhVien == null) data.Add("<li> Ma Sinh Vien is required</li>");
+                                if (a.matKhau == "" || a.matKhau == null) data.Add("<li> Mat Khau is required</li>");
+                                if (a.tenSinhVien == "" || a.tenSinhVien == null) data.Add("<li>Ten Sinh Vien is required</li>");
+                                data.Add("</ul>");
+                                data.ToArray();
+                                return Json(data, JsonRequestBehavior.AllowGet);
+                            }
+                        }
+                        catch (DbEntityValidationException ex)
+                        {
+                            foreach (var entityValidationErrors in ex.EntityValidationErrors)
+                            {
+                                foreach (var validationError in entityValidationErrors.ValidationErrors)
+                                {
+                                    Response.Write("Property: " + validationError.PropertyName + " Error: " + validationError.ErrorMessage);
+                                }
+                            }
+                        }
+                    }
+                    //deleting excel file from folder
+                    if ((System.IO.File.Exists(pathToExcelFile)))
+                    {
+                        System.IO.File.Delete(pathToExcelFile);
+                    }
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    //alert message for invalid file format
+                    data.Add("<ul>");
+                    data.Add("<li>Only Excel file format is allowed</li>");
+                    data.Add("</ul>");
+                    data.ToArray();
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                data.Add("<ul>");
+                if (FileUpload == null) data.Add("<li>Please choose Excel file</li>");
+                data.Add("</ul>");
+                data.ToArray();
+                return RedirectToAction("CreateExel");
+            }
         }
     }
 }
+
